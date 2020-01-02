ea369422d6affb7484b57e95c240556454c3d926
demola07 <45898468+demola07@users.noreply.github.com>
2020-01-02 14:06:29 +0100

    saved high scores to localstorage and sort highscores to take the highest five scors

--- end.js
+++ end.js
@@ -2,6 +2,9 @@ const username = document.querySelector('#username');
 const saveScoreButton = document.querySelector('#saveScoreButton');
 const finalScore = document.querySelector('#finalScore');
 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const MAX_HIGH_SCORE = 5;
+
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.textContent = mostRecentScore;
 
@@ -12,4 +15,18 @@ username.addEventListener('keyup', () => {
 saveHighScore = e => {
   e.preventDefault();
   console.log('clicked save button');
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  //Add score to array
+  highScores.push(score);
+  // sort it based on highscore
+  highScores.sort((a, b) => b.score - a.score);
+  //keep only the highest five scores
+  highScores.splice(5);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign('/');
 };
