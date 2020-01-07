5c13db607fdb0aa1e145f1fe01cdd78fed6018cf
demola07 <45898468+demola07@users.noreply.github.com>
2020-01-07 23:33:36 +0100

    using fetch to get questions from json file

--- game.js
+++ game.js
@@ -10,36 +10,22 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-  {
-    question: 'Inside which HTML element do we put the JavaScript??',
-    choice1: '<script>',
-    choice2: '<javascript>',
-    choice3: '<js>',
-    choice4: '<scripting>',
-    answer: 1
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4
-  }
-];
+let questions = [];
+
+fetch('questions.json')
+  .then(res => {
+    return res.json();
+  })
+  .then(loadQuestions => {
+    console.log(loadQuestions);
+    questions = loadQuestions;
+    startGame();
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
 //Constants
-
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
@@ -104,5 +90,3 @@ incrementScore = num => {
   score += num;
   scoreText.textContent = score;
 };
-
-startGame();
